9c842761a26b228fbf22484315f653a93d1bfbf0
James Lenahan <james.k.lenahan@gmail.com>
2020-04-06 20:46:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ class Breakfast {
 }
 class Lunch {
   constructor(salad, soup, drink) {
-    
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
   }
-  
 }
+
+class Dinner {
+  constructor
